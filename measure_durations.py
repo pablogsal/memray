d33b8ed5ed0322cfb7dd338d80a7ead46ee225d1
Pablo Galindo <pablogsal@gmail.com>
2022-05-13 20:34:47 +0100

    WIP

--- measure_durations.py
+++ measure_durations.py
@@ -4,125 +4,24 @@ import re
 
 DURATION_RE = re.compile(r"Total duration: (\d+) ms", re.IGNORECASE)
 
-"""
-Cachegrind performance metrics.
-"""
-
-from typing import List, Dict
-from subprocess import check_call, check_output
-from tempfile import NamedTemporaryFile
-
-
-def _run(args_list: List[str]) -> Dict[str, int]:
-    """
-    Run the the given program and arguments under Cachegrind, parse the
-    Cachegrind specs.
-
-    For now we just ignore program output, and in general this is not robust.
-    """
-    ARCH = check_output(["uname", "-m"]).strip()
-    temp_file = NamedTemporaryFile("r+")
-    check_call(
-        [
-            # Disable ASLR:
-            "setarch",
-            ARCH,
-            "-R",
-            "valgrind",
-            "--tool=cachegrind",
-            # Set some reasonable L1 and LL values, based on Haswell. You can set
-            # your own, important part is that they are consistent across runs,
-            # instead of the default of copying from the current machine.
-            "--I1=32768,8,64",
-            "--D1=32768,8,64",
-            "--LL=8388608,16,64",
-            "--cachegrind-out-file=" + temp_file.name,
-        ]
-        + args_list
-        , stderr=subprocess.DEVNULL
-        , stdout=subprocess.DEVNULL
-    )
-    return parse_cachegrind_output(temp_file)
-
-
-def parse_cachegrind_output(temp_file):
-    # Parse the output file:
-    lines = iter(temp_file)
-    for line in lines:
-        if line.startswith("events: "):
-            header = line[len("events: ") :].strip()
-            break
-    for line in lines:
-        last_line = line
-    assert last_line.startswith("summary: ")
-    last_line = last_line[len("summary:") :].strip()
-    return dict(zip(header.split(), [int(i) for i in last_line.split()]))
-
-
-def get_counts(cg_results: Dict[str, int]) -> Dict[str, int]:
-    """
-    Given the result of _run(), figure out the parameters we will use for final
-    estimate.
-
-    We pretend there's no L2 since Cachegrind doesn't currently support it.
-
-    Caveats: we're not including time to process instructions, only time to
-    access instruction cache(s), so we're assuming time to fetch and run
-    instruction is the same as time to retrieve data if they're both to L1
-    cache.
-    """
-    result = {}
-    d = cg_results
-
-    ram_hits = d["DLmr"] + d["DLmw"] + d["ILmr"]
-
-    l3_hits = d["I1mr"] + d["D1mw"] + d["D1mr"] - ram_hits
-
-    total_memory_rw = d["Ir"] + d["Dr"] + d["Dw"]
-    l1_hits = total_memory_rw - l3_hits - ram_hits
-    assert total_memory_rw == l1_hits + l3_hits + ram_hits
-
-    result["l1"] = l1_hits
-    result["l3"] = l3_hits
-    result["ram"] = ram_hits
-
-    return result
-
-
-def combined_instruction_estimate(counts: Dict[str, int]) -> int:
-    """
-    Given the result of _run(), return estimate of total time to run.
-
-    Multipliers were determined empirically, but some research suggests they're
-    a reasonable approximation for cache time ratios.  L3 is probably too low,
-    but then we're not simulating L2...
-    """
-    return counts["l1"] + (5 * counts["l3"]) + (35 * counts["ram"])
-
-
-def benchmark(args_list: List[str]) -> Dict[str, int]:
-    """Run the program, return dictionary with raw data and summary metric."""
-    result = _run(args_list)
-    counts = get_counts(result)
-    result["Overall"] = combined_instruction_estimate(counts)
-    return result
-
 with open("tests.txt") as testsfile:
     tests = [test.strip() for test in testsfile.readlines()]
 
 results = []
 
-for test in tests[:10]:
+for test in tests:
     print(test)
     try:
-        output = benchmark([sys.executable, "-m", "test", test])
-        memray_output = benchmark([sys.executable, "-m", "memray", "run", "-fo", "/dev/null", "-m", "test", test])
-    except subprocess.SubprocessError:
-        pass
-    results.append((test, output, memray_output))
+        output = subprocess.check_output([sys.executable, "-m", "test", test], text=True, stderr=subprocess.STDOUT)
+        memray_output = subprocess.check_output([sys.executable, "-m", "memray", "run", "-fo", "/dev/null", "-m", "test", test], text=True, stderr=subprocess.STDOUT)
+    except Exception as e:
+        print(f"Oh no: {e.}")
+        continue
+    match = DURATION_RE.findall(output)
+    memray_match = DURATION_RE.findall(memray_output)
+    if match and memray_match:
+        results.append((test, int(match[0]), int(memray_match[0])))
 
 with open("results_benchmark.txt", "w") as result_file:
     for test, before, after in results:
-        before = before["Overall"]
-        after = after["Overall"]
         result_file.write(f"{test},{before},{after},{(after/before)*100}\n")
