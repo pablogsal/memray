588f8971354b9a8f46a25e9c20d170fef30d57ea
Pablo Galindo Salgado <pablogsal@gmail.com>
2025-11-29 02:33:53 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add tests for the ghost unwinding

--- src/memray/_memray/ghost_stack/src/aarch64_darwin_trampoline.s
+++ src/memray/_memray/ghost_stack/src/aarch64_darwin_trampoline.s
@@ -9,14 +9,13 @@
 .globl _ghost_ret_trampoline
 .private_extern _ghost_ret_trampoline
 _ghost_ret_trampoline:
-.cfi_startproc
-.cfi_personality 0x9b, L_personality_ptr
-.cfi_lsda 0x1b, LLSDA0
+.cfi_startproc simple
+.cfi_signal_frame
+.cfi_def_cfa sp, 0
 .cfi_undefined lr
 
 LEHB0:
     sub sp, sp, #64
-.cfi_def_cfa_offset 64
     stp x0, x1, [sp, #0]
     stp x2, x3, [sp, #16]
     stp x4, x5, [sp, #32]
@@ -33,49 +32,19 @@ LEHB0:
     ldp x4, x5, [sp, #32]
     ldp x6, x7, [sp, #48]
     add sp, sp, #64
-.cfi_def_cfa_offset 0
 
     ret
+LEHE0:
 
 L3:
     bl _ghost_exception_handler
     mov lr, x0
     b ___cxa_rethrow
 
-Ltrampoline_end:
-    nop  /* padding to ensure proper size */
 .cfi_endproc
 
 .globl _ghost_ret_trampoline_start
 .private_extern _ghost_ret_trampoline_start
 .set _ghost_ret_trampoline_start, _ghost_ret_trampoline
-
-/* Indirect personality function pointer */
-.section __DATA,__data
-.p2align 3
-L_personality_ptr:
-    .quad ___gxx_personality_v0
-
-/* LSDA */
-.section __TEXT,__gcc_except_tab
-.p2align 2
-LLSDA0:
-    .byte 0xff
-    .byte 0x9b
-    .uleb128 LLSDATT0-LLSDATTD0
-LLSDATTD0:
-    .byte 0x1
-    .uleb128 LLSDACSE0-LLSDACSB0
-LLSDACSB0:
-    .uleb128 LEHB0-_ghost_ret_trampoline
-    .uleb128 L3-LEHB0
-    .uleb128 L3-_ghost_ret_trampoline
-    .uleb128 0x1
-LLSDACSE0:
-    .byte 0x1
-    .byte 0
-    .p2align 2
-    .long 0
-LLSDATT0:
 
 .subsections_via_symbols
